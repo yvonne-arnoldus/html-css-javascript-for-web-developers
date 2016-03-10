ac6ea38ab103cb2baddc6558cae497b9e35e782f
yvonne-arnoldus <yvonnearnoldus@zonnet.nl>
2016-03-10 10:59:14 +0100

    module 4 update

--- module4-solution/js/script.js
+++ module4-solution/js/script.js
@@ -117,7 +117,7 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
                 // it into the home html snippet.
                 // 
                 // var homeHtmlToInsertIntoMainPage = ....
-                var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", "{{" + chosenCategoryShortName + "}}");
+                var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", "'" + chosenCategoryShortName + "'");
 
 
                 // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
